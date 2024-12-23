64cc15da3d3f56d564beac5361b54f2833a2f36b
dilahazalbilgin <dilahazalbilgin@gmail.com>
2024-12-23 15:58:31 +0300

    SocialNetwork

---
+++ Documents/NetBeansProjects/MiniSocialNetwork/src/minisocialnetwork/UserManagement.java
@@ -0,0 +1,58 @@
+package minisocialnetwork;
+
+import java.util.*;
+
+public class UserManagement {
+    protected int ID;
+    protected String name;
+    protected List<UserManagement>friends;
+    protected LinkedList<Post> posts;
+    protected PriorityQueue<String> activityFeed;
+
+    
+    public UserManagement(int ID,String name){
+    this.name=name;
+    this.ID=ID;
+    this.friends=new ArrayList<>();
+    this.posts=new LinkedList<>();
+    this.activityFeed = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+    
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<UserManagement> getFriends() {
+        return friends;
+    }
+
+    public Queue<Post> getPosts() {
+        return posts;
+    }
+    
+    public PriorityQueue<String> getActivityFeed() {
+        return activityFeed;
+    }
+   
+    
+   public void addFriend(UserManagement friend) {
+        friends.add(friend);
+    }
+
+    public void removeFriend(UserManagement friend) {
+        friends.remove(friend);
+    }
+
+    public void addPost(Post post) {
+        posts.addFirst(post);
+        activityFeed.add("New Post: " + post.getContent() + " at " + post.getTimestap());
+    }
+
+    public void addActivity(String activity) {
+        activityFeed.add(activity);
+    }
+}
